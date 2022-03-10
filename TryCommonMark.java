1d9347f36c523258bf999c9db5e44a41c5b9b864
zfxd <zhang_weiyao@yahoo.com.sg>
2022-03-10 09:26:11 -0800

    print document

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -8,5 +8,6 @@ class TryCommonMark {
         Node document = parser.parse("This is *Sparta*");
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        System.out.println(document);
     }
 }
