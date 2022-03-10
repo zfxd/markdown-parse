958c9ca3c89b523167432df616ffbc1ebe54d8a5
zfxd <zhang_weiyao@yahoo.com.sg>
2022-03-10 09:56:14 -0800

    CommonMark into MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,35 +3,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        System.out.println("I made a change!!!");
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1)
-            {
-                break;
-            }
-            if(nextOpenBracket != 0)
-            {
-                if(markdown.charAt(nextOpenBracket-1) == '!')
-                {
-                    currentIndex = nextOpenBracket+1;
-                    continue;
-                }
-            }
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }
-        return toReturn;
+        Parser parser = Parser.builder().build();
+        Node node = parser.parse(markdown);
+        LinkVisitor visitor = new LinkVisitor();
+        node.accept(visitor);
+        System.out.println(visitor.linkCount);
+        System.out.println(visitor.links);
+        return visitor.links;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
